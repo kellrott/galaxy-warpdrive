b7d50e4de204e501ef555c2b02a7ea93326cebce
Kyle Ellrott <kellrott@gmail.com>
2014-10-26 11:46:22 -0700

    Adding status check and '-v' for verbose flag

--- warpdrive.py
+++ warpdrive.py
@@ -10,8 +10,6 @@ import subprocess
 import logging
 import requests
 
-logging.basicConfig(level=logging.INFO)
-
 def which(file):
     for path in os.environ["PATH"].split(":"):
         p = os.path.join(path, file)
@@ -55,7 +53,7 @@ def call_docker_run(
 
     logging.info("executing: " + " ".join(cmd))
     proc = subprocess.Popen(cmd, close_fds=True, env=sys_env, stdout=subprocess.PIPE)
-    stderr, stdout = proc.communicate()
+    stdout, stderr = proc.communicate()
     if proc.returncode != 0:
         raise Exception("Call Failed: %s" % (cmd))
 
@@ -77,7 +75,7 @@ def call_docker_kill(
 
     logging.info("executing: " + " ".join(cmd))
     proc = subprocess.Popen(cmd, close_fds=True, env=sys_env, stdout=subprocess.PIPE)
-    stderr, stdout = proc.communicate()
+    stdout, stderr = proc.communicate()
     if proc.returncode != 0:
         raise Exception("Call Failed: %s" % (cmd))
 
@@ -98,9 +96,32 @@ def call_docker_rm(
 
     logging.info("executing: " + " ".join(cmd))
     proc = subprocess.Popen(cmd, close_fds=True, env=sys_env, stdout=subprocess.PIPE)
-    stderr, stdout = proc.communicate()
+    stdout, stderr = proc.communicate()
     if proc.returncode != 0:
         raise Exception("Call Failed: %s" % (cmd))
+
+
+
+def call_docker_ps(
+    host=None
+    ):
+
+    docker_path = get_docker_path()
+
+    cmd = [
+        docker_path, "ps", "-a", "--no-trunc", "-s"
+    ]
+
+    sys_env = dict(os.environ)
+    if host is not None:
+        sys_env['DOCKER_HOST'] = host
+
+    logging.info("executing: " + " ".join(cmd))
+    proc = subprocess.Popen(cmd, close_fds=True, env=sys_env, stdout=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception("Call Failed: %s" % (cmd))
+    return stdout
 
 
 def run_up(args):
@@ -147,9 +168,37 @@ def run_down(args):
         )
 
 
+def run_status(args):
+    txt = call_docker_ps(
+        host=args.host
+    )
+
+    lines = txt.split("\n")
+
+    containerIndex = lines[0].index("CONTAINER ID")
+    imageIndex = lines[0].index("IMAGE")
+    commandIndex = lines[0].index("COMMAND")
+    portsIndex = lines[0].index("PORTS")
+    statusIndex = lines[0].index("STATUS")
+    namesIndex = lines[0].index("NAMES")
+    sizeIndex = lines[0].index("SIZE")
+
+    found = False
+    for line in lines[1:]:
+        if len(line):
+            name = line[namesIndex:sizeIndex].split()[0]
+            status = line[statusIndex:portsIndex].split()[0]
+            if name == args.name:
+                print status
+                found = True
+    if not found:
+        print "NotFound"
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", action="store_true", default=False)
 
     subparsers = parser.add_subparsers(title="subcommand")
 
@@ -160,6 +209,7 @@ if __name__ == "__main__":
     parser_up.add_argument("-w", "--work-dir", default="/tmp")
     parser_up.add_argument("-p", "--port", default="8080")
     parser_up.add_argument("-n", "--name", default="galaxy")
+    parser_up.add_argument("-c", "--child", action="store_true", help="Launch jobs in child containers", default=False)
     parser_up.add_argument("--key", default="HSNiugRFvgT574F43jZ7N9F3")
     parser_up.add_argument("--host", default=None)
     parser_up.set_defaults(func=run_up)
@@ -170,8 +220,13 @@ if __name__ == "__main__":
     parser_down.add_argument("--host", default=None)
     parser_down.set_defaults(func=run_down)
 
-
-
+    parser_status = subparsers.add_parser('status')
+    parser_status.add_argument("-n", "--name", default="galaxy")
+    parser_status.add_argument("--host", default=None)
+    parser_status.set_defaults(func=run_status)
 
     args = parser.parse_args()
+
+    if args.v:
+        logging.basicConfig(level=logging.INFO)
     sys.exit(args.func(args))
